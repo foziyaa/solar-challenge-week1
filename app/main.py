b60fdea1fa5d494fd4bc2805b752a73ad3a165d4
foziyaa <142108706+foziyaa@users.noreply.github.com>
2025-05-21 22:12:48 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 import pandas as pd
-from utils import generate_dummy_data, plot_bar_chart, plot_line_chart
+from app.utils import generate_dummy_data
 
 
 # Set Streamlit page configuration
